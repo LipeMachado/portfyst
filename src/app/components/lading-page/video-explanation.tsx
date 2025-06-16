77b49db403a8fb0f392ca69a2914988064995e18
LipeMachado <silvalipe765@gmail.com>
2025-06-16 18:16:55 -0300

    Update style

--- src/app/components/lading-page/video-explanation.tsx
+++ src/app/components/lading-page/video-explanation.tsx
@@ -2,8 +2,8 @@ import { Play } from "lucide-react";
 
 export default function VideoExplanation() {
   return (
-    <div className="border rounded-xl border-border-primary aspect-video flex items-center justify-center my-20">
-      <Play className="text-accent-purple size-16" />
+    <div className="border rounded-xl border-border-secondary aspect-video flex items-center justify-center my-20 group/item cursor-pointer">
+      <Play className="text-accent-purple size-16 group-hover/item:scale-110" />
     </div>
   );
 }
