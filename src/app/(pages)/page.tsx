0622e8ff1063b9bc60e6bf284b3f07469d5c3834
LipeMachado <silvalipe765@gmail.com>
2025-06-16 12:26:16 -0300

    Move archives

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,8 +1,8 @@
-import Header from "@/components/lading-page/header";
+import Header from "@/app/components/lading-page/header";
 import Hero from "../components/lading-page/hero";
-import VideoExplanation from "@/components/lading-page/video-explanation";
-import Pricing from "@/components/lading-page/pricing";
-import Faq from "@/components/lading-page/faq";
+import VideoExplanation from "@/app/components/lading-page/video-explanation";
+import Pricing from "@/app/components/lading-page/pricing";
+import Faq from "@/app/components/lading-page/faq";
 
 export default function Home() {
   return (
