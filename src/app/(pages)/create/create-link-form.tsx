9ed41b785541a0b78628988a7d287d9a48d51c07
LipeMachado <silvalipe765@gmail.com>
2025-06-16 12:41:01 -0300

    Move and add page create

---
+++ src/app/(pages)/create/create-link-form.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { createLink } from "@/actions/create-link";
+import { verifyLink } from "@/actions/verify-link";
+import Button from "@/components/ui/button";
+import TextInput from "@/components/ui/text-input";
+import { sanitizeLink } from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+
+export default function CreateLinkForm() {
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+
+  const [link, setLink] = useState(
+    sanitizeLink(searchParams.get("link") || "")
+  );
+  const [error, setError] = useState("");
+
+  function handleLinkChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setLink(sanitizeLink(e.target.value));
+    setError("");
+  }
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    // Quando o usuario nao escreve um link
+    if (link.length === 0) return setError("Escolha um link primeiro :)");
+
+    const isLinkTaken = await verifyLink(link);
+
+    if (isLinkTaken) return setError("Desculpe, esse link já está em uso.");
+
+    const isLinkCreated = await createLink(link);
+
+    if (!isLinkCreated)
+      return setError("Erro ao criar o perfil. Tente novamente.");
+
+    router.push(`/${link}`);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="w-full flex items-center gap-2">
+        <span>projectinbio.com/</span>
+        <TextInput value={link} onChange={handleLinkChange} />
+        <Button className="w-[126px]">Criar</Button>
+      </form>
+      <div>
+        <span className="text-accent-pink">{error}</span>
+      </div>
+    </>
+  );
+}
---
+++ src/app/(pages)/create/page.tsx
@@ -0,0 +1,24 @@
+
+import Header from "@/components/lading-page/header";
+import { Rocket } from "lucide-react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "ProjectInBio - Criar",
+  description: "ProjectInBio - Criar",
+};
+
+export default function CriarPage() {
+
+  return (
+    <div>
+      <Header />
+      <div className="h-screen flex flex-col gap-10 items-center justify-center max-w-xl mx-auto">
+        <div className="flex items-center gap-4">
+          <h1 className="text-4xl font-bold text-white">Escolha seu link</h1>
+          <Rocket className="size-10" />
+        </div>
+      </div>
+    </div>
+  );
+}
