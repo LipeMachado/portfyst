bbdba7fda5e78396befe2dbb7e65cfd1b3891e72
LipeMachado <silvalipe765@gmail.com>
2025-06-16 12:21:46 -0300

    Add pricing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Header from "@/components/lading-page/header";
 import Hero from "../components/lading-page/hero";
+import VideoExplanation from "@/components/lading-page/video-explanation";
+import Pricing from "@/components/lading-page/pricing";
 
 export default function Home() {
   return (
@@ -7,6 +9,8 @@ export default function Home() {
       <div className="max-w-7xl mx-auto">
         <Header />
         <Hero />
+        <VideoExplanation />
+        <Pricing />
       </div>
     </>
   );
