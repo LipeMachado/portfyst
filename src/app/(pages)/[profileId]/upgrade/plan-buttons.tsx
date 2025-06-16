6f8e6f628fc0f697e28751a6e9725d619fa227df
LipeMachado <silvalipe765@gmail.com>
2025-06-16 12:57:18 -0300

    Add plan page

---
+++ src/app/(pages)/[profileId]/upgrade/plan-buttons.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Button from "@/components/ui/button";
+import { useParams } from "next/navigation";
+
+export default function PlanButtons() {
+  const { profileId } = useParams();
+
+  return (
+    <div className="flex gap-4">
+      <Button
+      >
+        R$ 9,90 / mês
+      </Button>
+      <Button
+
+      >
+        R$ 99,90 Vitalício
+      </Button>
+    </div>
+  );
+}
