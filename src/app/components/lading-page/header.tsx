f9a85e46c2ba9cb4809de9b8dfcafd3447bac210
LipeMachado <silvalipe765@gmail.com>
2025-06-24 20:14:22 -0300

    Change name

--- src/app/components/lading-page/header.tsx
+++ src/app/components/lading-page/header.tsx
@@ -1,7 +1,11 @@
+import { auth } from "@/app/lib/auth";
 import Button from "../ui/button";
 import Link from "next/link";
 
 export default async function Header() {
+  const session = await auth()
+
+  console.log(session)
 
   return (
     <div className="absolute top-0 left-0 right-0 max-w-7xl mx-auto flex items-center justify-between py-10">
