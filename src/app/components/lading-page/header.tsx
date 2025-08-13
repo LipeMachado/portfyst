1ef94eecd5a29a0d647e697204d64416b5d77cab
LipeMachado <silvalipe765@gmail.com>
2025-08-12 23:26:35 -0300

    Adicionar autenticação ao cabeçalho da página inicial

--- src/app/components/lading-page/header.tsx
+++ src/app/components/lading-page/header.tsx
@@ -1,8 +1,10 @@
+import { auth } from "@/app/lib/auth";
 import Button from "../ui/button";
 import Link from "next/link";
 
 export default async function Header() {
 
+  const session = auth()
   return (
     <div className="absolute top-0 left-0 right-0 max-w-7xl mx-auto flex items-center justify-between py-10">
       <div className="flex items-center gap-4">
